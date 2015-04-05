27ae9d2ad2abf8404f9b7d9c0b807e763127c171
sam <sam@cafe.tg>
2015-04-05 10:09:08 +0200

    Génération d'une documentation technique
    Signed-off-by: sam <sam@cafe.tg>

---
+++ vues/v_suiviFrais.php
@@ -0,0 +1,154 @@
+<div id="contenu">
+    <h2> Valider les fiches de frais  </h2>
+    <h3>Visiteur et mois à sélectionner</h3>
+    <form action="index.php?uc=suiviFraisFiche&action=miseEnPaiement" method="POST">
+        <div class="corpsForm">
+            <p>
+            <label for="lstVisiteur">Visiteur :</label>
+            <select name="lstVisiteur">
+                <?php
+                // parcour du tableau associatif lesVisiteur pour remplir la liste des visiteur
+                    foreach ($lesVisiteurs as $unVisiteur) {
+                        // récupération du nom dans la variable $nom
+                        $nom = $unVisiteur['nom'];
+                        // récupération du prénom dans la variable $prenom
+                        $prenom = $unVisiteur['prenom'];
+                        // récupération de l'id dans la variable $id
+                        $id = $unVisiteur['id'];
+                        if ($nom." ".$prenom == $leVisiteur['nom']." ".$leVisiteur['prenom']) {                                                      
+                        ?>
+                        <option selected value="<?php echo  $id ;  ?>"><?php echo  $nom." ".$prenom ;  ?> </option>
+                        <?php
+                         } else {
+                        ?> 
+                         <option  value="<?php echo  $id ; ?>"><?php echo  $nom." ".$prenom ; ?> </option>
+                        <?php
+                         }
+                    }
+                    ?>    
+            </select>
+            </p>
+            <p>
+            <label for="lstMois" accesskey="n">Mois : </label>
+            <select id="lstMois" name="lstMois"> 
+            
+            <?php
+            // Boucle pour le parcour du tableau associatif $lesMois
+			foreach ($lesMois as $unMois)
+			{
+                            //récupération du mois au format aa/mmmm
+			    $mois = $unMois['numMois']."/".$unMois['numAnnee'];
+                            $numAnnee =  $unMois['numAnnee'];
+                            $numMois =  $unMois['numMois'];
+                            // condition pour savoir si le mois à la position t est égale au mois choisi
+                            if($mois == $moisASelectionner){
+                            ?>
+                            <option selected value="<?php echo $mois ; ?>"><?php echo  $numMois."/".$numAnnee ; ?> </option>
+                            <?php 
+                            } else { ?>
+                            <option value="<?php echo $mois ; ?>"><?php echo  $numMois."/".$numAnnee ; ?> </option>
+                            <?php 
+                            }	
+			}        
+		   ?>         
+        </select>
+            </p>          
+        </div>
+        <div class="piedForm">
+         <p>
+            <input id="ok" type="submit" value="Valider" size="20" />
+         </p> 
+      </div>
+    </form> 
+    <h3>Détail de la fiche de frais : <?php echo $leVisiteur['nom']." ".$leVisiteur['prenom'] ?> - 
+    <?php echo $moisASelectionner ?> </h3>
+   
+    Etat: <?php echo $infoVisiteur['libEtat'] ; ?> depuis le <?php echo dateAnglaisVersFrancais($infoVisiteur['dateModif']) ; ?> <br />
+    Montant validé : <?php echo $infoVisiteur['montantValide'] ; ?>
+  
+    <form method="POST"  action="index.php?uc=validerFraisFiche&action=validerMajFraisForfait">
+      <div class="corpsForm">
+          <input type="hidden" name="idVis" value="<?php echo $idVisiteur ; ?>" />
+           <input type="hidden" name="leMois" value="<?php echo $moisAng ;  ?>" />
+          <fieldset>
+            <legend>Eléments forfaitisés
+            </legend>
+			<?php
+                       
+				foreach ($lesFraisForfait as $unFrais)
+				{
+					$idFrais = $unFrais['idfrais'];
+					$libelle = $unFrais['libelle'];
+					$quantite = $unFrais['quantite'];
+                                        $sommeFF += $quantite ;
+			?>
+					<p>
+						<label for="idFrais"><?php echo $libelle ; ?></label>
+						<input type="text" id="idFrais" name="lesFrais[<?php echo $idFrais ; ?>]" size="10" maxlength="5" value="<?php echo $quantite ; ?>" >
+					</p>
+			
+			<?php
+				}
+			?>
+			 
+          </fieldset>
+          <h3>TOTAL DES ELEMENTS FORFAITISES : <?php echo $sommeFF ; ?></h3>
+      </div>
+        
+    </form> 
+    
+    <div class="corpsForm"> 
+        <input type="hidden" name="idVis" value="<?php echo $idVisiteur ; ?>" />
+           <input type="hidden" name="leMois" value="<?php echo $moisAng ; ?>" />
+      <table class="listeLegere">
+  	   <caption>Descriptif des éléments hors forfait
+       </caption>
+             <tr>
+               <th class="date">Date</th>
+				<th class="libelle">Libellé</th>  
+                <th class="montant">Montant</th>  
+                <th class="action">&nbsp;</th>   
+             </tr>
+          
+    <?php   
+            if($ok = FALSE ) 
+             {
+                 ?> 
+             <table><i><tr> Aucun frais hors forfait pour ce mois </tr></i></table>
+             <?php } else {
+                  foreach ( $lesFraisHorsForfait as $unFraisHorsForfait) {
+			$libelle = $unFraisHorsForfait['libelle'];
+			$date = $unFraisHorsForfait['date'];
+			$montant=$unFraisHorsForfait['montant'];
+			$id = $unFraisHorsForfait['id'];
+                        $sommeFHF +=  $montant ;
+	     ?>
+             
+            <tr>
+                <td> <?php echo $date ;  ?></td>
+                <td><?php echo $libelle ; ?></td>
+                <td><?php echo $montant ; ?></td>
+ 
+             </tr>
+
+      <?php } 
+            
+       } ?></table> 		 
+        <h3>TOTAL DES FRAIS HORS FORFAITS : <?php echo $sommeFHF ; ?> </h3>  
+    </div> <br />
+
+        <div class="piedForm">
+            <?php if( $infoVisiteur['idEtat'] == "V") {
+                ?>           
+                <p><a href="index.php?uc=suiviFraisFiche&action=paiementFiche&idVisiteur=<?php echo $idVisiteur ; ?>&mois=<?php echo $moisAng ; ?>" 
+                      title="Mettre en paiement la fiche de frais">Mise en paiement</a>
+           <?php } elseif ($infoVisiteur['idEtat'] == "VA") {  ?>
+            
+            <a href="index.php?uc=suiviFraisFiche&action=rembourserFiche&idVisiteur=<?php echo $idVisiteur ; ?>&mois=<?php echo $moisAng ; ?>" 
+               title="Mettre la fiche de frais à l'état remboursée">Valider le paiement de cette fiche </a></p> 
+            <?php }else { ?>
+               <center> <h3>Etat de la fiche : <?php  echo $infoVisiteur['libEtat'] ;?> </h3</center>
+            <?php } ?>    
+        </div>
+     
+</div>  
