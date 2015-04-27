03c366d67374da56adc9a9b8526a8c80c57cd41a
sam <sam@cafe.tg>
2015-04-27 15:03:41 +0200

    Prise en compte du fait qu'une fiche de frais qui est outre que l'état CR ne peut être modifié

--- vues/v_validerFrais.php
+++ vues/v_validerFrais.php
@@ -1,66 +1,4 @@
 
-<div id="contenu">
-    <h2> Valider les fiches de frais  </h2>
-    <h3>Visiteur et mois à sélectionner</h3>
-    <form action="index.php?uc=validerFraisFiche&action=validerFrais" method="POST">
-        <div class="corpsForm">
-            <p>
-            <label for="lstVisiteur">Visiteur :</label>
-            <select name="lstVisiteur">
-                <?php 
-                     // parcour du tableau associatif les visiteurs
-                    foreach ($lesVisiteurs as $unVisiteur){
-                        // récupération du nom dans la variable $nom
-                        $nom = $unVisiteur['nom'];
-                        // récupération du prenom dans la variable $prenom
-                        $prenom = $unVisiteur['prenom'];
-                        // récupération de l'id dans la variable $id
-                        $id = $unVisiteur['id'];
-                        //condition pour savoir si le visiteur se trouvant à la position t est égale au visiteur choisi
-                        if ($nom." ".$prenom === $leVisiteur['nom']." ".$leVisiteur['prenom']) {                                                      
-                        ?>
-                        <option selected value="<?php echo  $id ; ?>"><?php echo  $nom." ".$prenom ; ?> </option>
-                        <?php
-                         } else {
-                        ?> 
-                         <option  value="<?php echo  $id ; ?>"><?php echo  $nom." ".$prenom ; ?> </option>
-                        <?php
-                         }
-                    }
-                    ?>    
-            </select>
-            </p>
-            <p>
-            <label for="lstMois" accesskey="n">Mois : </label>
-            <select id="lstMois" name="lstMois"> 
-            
-            <?php
-            // parcour du tableau associatif $lesMois
-			foreach ($lesMois as $unMois)
-			{
-                            // récupération du mois au format aa/mmmm
-			    $mois = $unMois['numMois']."/".$unMois['numAnnee'];
-                            $numAnnee =  $unMois['numAnnee'];
-                            $numMois =  $unMois['numMois'];
-                            if ($mois === $moisASelectionner) {
-                            ?>
-                                <option selected value="<?php echo $mois ; ?>"><?php echo  $numMois."/".$numAnnee ; ?> </option>
-                            <?php 
-				} else { ?>
-				<option value="<?php echo $mois ; ?>"><?php echo  $numMois."/".$numAnnee ; ?> </option>
-                            <?php 
-                                }	
-			}        
-		   ?>         
-        </select>
-            </p>          
-        </div>
-        <div class="piedForm">
-         <p>
-            <input id="ok" type="submit" value="Valider" size="20" />
-         </p> 
-      </div>
-    </form>  
    <h3>Détail de la fiche de frais : <?php echo $leVisiteur['nom']." ".$leVisiteur['prenom'] ;  ?> - 
    <?php echo $moisASelectionner ;  ?> </h3>
    
@@ -76,6 +14,7 @@
             <legend>Eléments forfaitisés
             </legend>
 			<?php
+                        if($lesInfosFicheFrais['idEtat'] === 'CR'){
                         // parcour du tableau associatif $lesFraisForfait
 				foreach ($lesFraisForfait as $unFrais)
 				{
@@ -91,17 +30,44 @@
 			
 			<?php
 				}
+                        } else { ?>
+                           <table class="listeLegere">
+                            <tr>
+                        <?php
+                        foreach ( $lesFraisForfait as $unFraisForfait ) {
+                            $libelle = $unFraisForfait['libelle'];
+                        ?>	
+			<th> <?php echo $libelle ; ?></th>
+                        <?php
+                            }
+                        ?>
+                        </tr>
+                        <tr>
+                        <?php
+                            foreach (  $lesFraisForfait as $unFraisForfait  ) {
+				$quantite = $unFraisForfait['quantite'];
+                                $sommeFF += $quantite ;
+                        ?>
+                        <td class="qteForfait"><?php echo $quantite ; ?> </td>
+                        <?php
+                            }
+                        ?>
+                        </tr>
+                        </table> 
+                       <?php }
 			?>
 			 
           </fieldset>
           <h3>TOTAL DES ELEMENTS FORFAITISES : <?php echo $sommeFF ; ?></h3>
       </div>
+        <?php if($lesInfosFicheFrais['idEtat'] === 'CR'){ ?>
       <div class="piedForm">
       <p>
         <input id="ok" type="submit" value="Valider" size="20" name="valid" />
         <input id="annuler" type="reset" value="Effacer" size="20" />
       </p> 
-      </div>     
+      </div> 
+        <?php } ?>
     </form> 
     <!-- Affichage des informations liée aux élements fors forfaitisés -->
     <div class="corpsForm"> 
@@ -112,8 +78,9 @@
              <tr>
                 <th class="date">Date</th>
 				<th class="libelle">Libellé</th>  
-                <th class="montant">Montant</th>  
-                <th class="action"><?php espace() ;?></th>   
+                <th class="montant">Montant</th>
+                <?php if($lesInfosFicheFrais['idEtat'] === 'CR'){ ?>
+                <th class="action"><?php espace() ;?></th><?php } ?>   
               
              </tr>
           
@@ -141,7 +108,7 @@
                 <td> <?php echo $date ; ?></td>
                 <td><?php echo $libelle ; ?></td>
                 <td><?php echo $montant ; ?></td>
-                <?php if(substr($libelle, 0 , 6) != "REFUSE"){ ?>
+                <?php if((substr($libelle, 0 , 6) !== "REFUSE")&& ($lesInfosFicheFrais['idEtat'] === 'CR')){ ?>
                 <td><a href="index.php?uc=validerFraisFiche&action=supprimerFrais&idFrais=<?php echo $id ; ?>&idVis=<?php echo $idVisiteur ; ?>&moisAng=<?php echo $moisAng ; ?>" 
 				onclick="return confirm('Voulez-vous vraiment supprimer ce frais?');">Supprimer ce frais</a></td>
                  <?php } ?>                             
@@ -157,18 +124,29 @@
                 <input type="hidden" name="idVis" value="<?php echo $idVisiteur ; ?>" />
                 <input type="hidden" name="leMois" value="<?php echo $moisAng  ; ?>" />
                 <p>
+                    
+                    <?php if($lesInfosFicheFrais['idEtat'] === 'CR') { ?>
                     <label for="nbJustificatif"> Nombre de justificatifs fournis :</label>
                     <input type="text" name="nbJustificatif" value="<?php echo $nbJustificatifs ; ?>" size="5" maxlength="5">
+                 <h1><?php } else { 
+                    espace(2) ?> Nombre de justificatifs fournis : <?php
+                        echo $nbJustificatifs ;
+                    }
+                    ?></h1>
+                    
                 </p>
                 </div>
+                <?php if($lesInfosFicheFrais['idEtat'] === 'CR'){ ?>
         <div class="piedForm">
       <p>
         <input id="ok" type="submit" value="Valider" size="20" />
         <input id="annuler" type="reset" value="Effacer" size="20" />
       </p>
       </div>
+                <?php } ?>
     <br />
      </form>
+      <?php if($lesInfosFicheFrais['idEtat'] === 'CR') { ?>  
      <form method="POST"  action="index.php?uc=validerFraisFiche&action=validerFiche">
         <div class="corpsForm">
             <input type="hidden" name="idVis" value="<?php echo $idVisiteur ; ?>" />
@@ -179,5 +157,6 @@
         <div class="piedForm">
          <p><input id="ok" type="submit" value="Valider cette fiche" size="40" /></p> 
         </div>
+       <?php } ?>  
      </form>        
 </div>
