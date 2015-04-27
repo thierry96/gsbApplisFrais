e2005e71d27bdc8480655ffa28d2d0dad3fadc5f
sam <sam@cafe.tg>
2015-04-27 15:11:04 +0200

    Intégration du nouveau fichier javascript à la page.

--- vues/v_entete.php
+++ vues/v_entete.php
@@ -5,7 +5,7 @@
     <title>Intranet du Laboratoire Galaxy-Swiss Bourdin</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <link href="./styles/styles.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="javascript.js"></script>
+    <script type="text/javascript" src="./include/code.lib.js"></script>
     <link rel="shortcut icon" type="image/x-icon" href="./images/logo.jpg" />
   </head>
   <body>
